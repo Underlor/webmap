a436e350fd6ca37e7c2ac457e36a763528e7e228
Underlor <Cfyz_74@mail.ru>
2017-11-21 15:23:39 +0500

    Сууука

--- pcinfo/views.py
+++ pcinfo/views.py
@@ -25,33 +25,35 @@ class PcinfoTable(TemplateView):
 
         PCs = []
         for file in files:
-            123
-            if file.split(".")[len(file.split(".")) - 1] == "txt":
-                i += 1
-                PC = []
-                name = file
-                filechanged = time.ctime(os.path.getmtime(path + "\\" + str(file)))
-                f = open(path + "\\" + str(file))
-                file = []
-                for line in f:
-                    file.append(line)
-                # print(str(i) + ". " + f.name + "    |   " + file[2][file[2].rfind(':') + 1:len(file[2]) - 1])
-                try:
-                    PC.append(i)
-                    PC.append(file[0][file[0].rfind(':') + 1:len(file[0]) - 1])
-                    PC.append(file[2][file[2].rfind(':') + 1:len(file[2]) - 1])
-                    PC.append(file[3][file[3].rfind(':') + 1:len(file[3]) - 1])
-                    PC.append(file[4][file[4].rfind(':') + 1:len(file[4]) - 1])
-                    PC.append(dparser.parse(file[5][file[5].rfind(':') + 1:len(file[5]) - 1]))
-                    PC.append(int(file[14][file[14].rfind(':') + 1:len(file[14]) - 1]))
-                    PC.append(file[11][file[11].rfind(':') + 1:len(file[11]) - 1])
-                    PC.append(dparser.parse(filechanged))
-                    PC.append(name)
-                except Exception as e:
-                    print(e)
-                PCs.append(PC)
-                f.close()
-                # PC['IP'] =
+            try:
+                if file.split(".")[len(file.split(".")) - 1] == "txt":
+                    i += 1
+                    PC = []
+                    name = file
+                    filechanged = time.ctime(os.path.getmtime(path + "\\" + str(file)))
+                    f = open(path + "\\" + str(file))
+                    file = []
+                    for line in f:
+                        file.append(line)
+                    # print(str(i) + ". " + f.name + "    |   " + file[2][file[2].rfind(':') + 1:len(file[2]) - 1])
+                    try:
+                        PC.append(i)
+                        PC.append(file[0][file[0].rfind(':') + 1:len(file[0]) - 1])
+                        PC.append(file[2][file[2].rfind(':') + 1:len(file[2]) - 1])
+                        PC.append(file[3][file[3].rfind(':') + 1:len(file[3]) - 1])
+                        PC.append(file[4][file[4].rfind(':') + 1:len(file[4]) - 1])
+                        PC.append(dparser.parse(file[5][file[5].rfind(':') + 1:len(file[5]) - 1]))
+                        PC.append(int(file[14][file[14].rfind(':') + 1:len(file[14]) - 1]))
+                        PC.append(file[11][file[11].rfind(':') + 1:len(file[11]) - 1])
+                        PC.append(dparser.parse(filechanged))
+                        PC.append(name)
+                    except Exception as e:
+                        print(e)
+                    PCs.append(PC)
+                    f.close()
+                    # PC['IP'] =
+            except Exception as e:
+                pass
         PCsOut = []
         for i in range(len(PCs)):
             finder = False
